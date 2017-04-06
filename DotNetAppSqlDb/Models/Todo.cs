5839ff25dd5a7759cc075728feb93c58f74654cc
Cephas Lin <cephalin@microsoft.com>
2017-04-07 01:57:42 +0200

    add files

---
+++ DotNetAppSqlDb/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class Todo
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
